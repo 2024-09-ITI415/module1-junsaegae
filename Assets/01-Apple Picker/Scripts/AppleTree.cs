10ea34bc34b6475e5c102f138950b2d9a815d942
junsaegae <rykjun@gmail.com>
2024-09-21 01:16:16 -0400

    Changed Script for AppleTree to Drop Apples

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -9,21 +9,41 @@ public class AppleTree : MonoBehaviour
 
     public GameObject applePrefab;
 
-    public float speed = 1f;
+    public float speed = 10f;
 
     public float leftAndRightEdge = 10f;
 
-    public float chanceToChangeDirection;
+    public float chanceToChangeDirection = 0.1f;
 
-    public float secondBetweenAppleDrop;
+    public float secondBetweenAppleDrop = 1f;
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
     }
 
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject;
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenApple)
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge) {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge) {
+            speed = -Mathf.Abs(speed);
+        } else if (Random.value < chanceToChangeDirection) {
+            speed *= -1;
+        }
+    }
+    void FixedUpdate() {
+        if (Random.value < chanceToChangeDirection) {
+            speed *= -1;
+        }
     }
 }
