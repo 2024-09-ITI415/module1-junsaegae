802768fe4e767be00b822798dd9b93e210809d8b
junsaegae <rykjun@gmail.com>
2024-09-26 00:18:23 -0400

    Altered Scripts for Basket and HighScore

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -9,14 +9,16 @@ public class Apple : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (transform.position.y < bottomY) {
-            Destroy(this.gameObject);
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY) {
+            Destroy(this.gameObject);
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker> ();
+            apScript.AppleDestroyed();
+        }
     }
 }
