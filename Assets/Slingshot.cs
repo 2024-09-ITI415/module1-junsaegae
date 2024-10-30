ae7d90cde749dd055712932de2206b50f95c5956
junsaegae <rykjun@gmail.com>
2024-10-30 16:40:25 -0400

    Updated Slingshot Script

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -2,30 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slingshot : MonoBehaviour {
-    public GameObject launchPoint;
-
-     void Start()
-    {
+namespace MissionDemolition {  // Added namespace to avoid conflicts
+    public class Slingshot : MonoBehaviour {
+        public GameObject launchPoint;
         
-    }
-    void Awake () {
-        Transform launchPointTrans = transform.Find("LaunchPoint");
-        launchPoint = launchPointTrans.gameObject;
-        launchPoint.SetActive(false);
-    }
-   
-    void OnMouseEnter() {
-        //print("Slingshot:OnMouseEnter()");
-        launchPoint.SetActive(true);
-    }
-    void OnMouseExit() {
-        //print("Slingshot:OnMouseExit()");
-        launchPoint.SetActive(false);
-    }
-    // Update is called once per frame
-    void Update()
-    {
+        void Awake() {
+            Transform launchPointTrans = transform.Find("LaunchPoint");
+            launchPoint = launchPointTrans.gameObject;
+            launchPoint.SetActive(false);
+        }
         
+        void OnMouseEnter() {
+            //print("Slingshot:OnMouseEnter()");
+            launchPoint.SetActive(true);
+        }
+        
+        void OnMouseExit() {
+            //print("Slingshot:OnMouseExit()");
+            launchPoint.SetActive(false);
+        }
     }
 }
